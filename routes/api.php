edc08085a246963dcf9805c3a4d722bcf5993ca3
Adrián Fervenza <afervenza@synergie.es>
2024-07-13 22:15:23 +0200

    Add api test route

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 include_once('src/Tweets/Infrastructure/Http/Routes/Routes.php');
+
+Route::get('/test', function(Request $request){
+    return response()->json(['result' => 'OK']);
+});
