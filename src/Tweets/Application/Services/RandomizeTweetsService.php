0658a4eb6aabb720f943abd22ffc48ff2af1825c
Adrián Fervenza <afervenza@synergie.es>
2024-07-15 00:07:02 +0200

    Add fake opinion tweets local database and update randomizer

--- src/Tweets/Application/Services/RandomizeTweetsService.php
+++ src/Tweets/Application/Services/RandomizeTweetsService.php
@@ -7,12 +7,29 @@ use Illuminate\Support\Facades\DB;
 
 class RandomizeTweetsService
 {
+    const TWEETS_FILE_PATH = "/src/Tweets/Domain/Data/tweets.json";
+
+    private function updateTweetsContent()
+    {
+        $jsonDatabase = [...array_unique(json_decode(file_get_contents(base_path() . self::TWEETS_FILE_PATH)))];
+        shuffle($jsonDatabase);
+        $counter = 0;
+        foreach (Tweet::all() as $tweet) {
+            $newContent = $jsonDatabase[$counter];
+            $tweet->content = $newContent;
+            $tweet->save();
+            $counter++;
+        }
+    }
+
     public function __invoke()
     {
         try {
             DB::beginTransaction();
             Tweet::truncate();
             Tweet::factory()->count(1000)->create();
+
+            $this->updateTweetsContent();
 
             return true;
 
